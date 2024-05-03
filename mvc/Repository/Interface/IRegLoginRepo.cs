85511295ca791cbed872292f5389c7caa89ae5fb
“bandhankalkani” <bkalkani710@rku.ac.in>
2024-05-03 15:10:28 +0530

    reg

--- mvc/Repository/Interface/IRegLoginRepo.cs
+++ mvc/Repository/Interface/IRegLoginRepo.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using mvc.Models;
 
 namespace mvc.Repository.Interface
 {
     public interface IRegLoginRepo
     {
-
+        void Register(RegLoginModel reg);
+        bool Login(RegLoginModel login);
     }
 }
---
+++ mvc/Views/JS/Registration.js
@@ -0,0 +1,3 @@
+function validation(){
+    var username = document.getElementById('').value;
+}
